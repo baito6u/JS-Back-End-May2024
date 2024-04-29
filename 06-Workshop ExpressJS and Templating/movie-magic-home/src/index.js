2c892712ae5dfa975e6887a2f1ac556ee3161991
Todor Krumov <baito6u@gmail.com>
2024-04-29 05:07:22 +0300

    Setup express handlebars

--- 06-Workshop ExpressJS and Templating/movie-magic-home/src/index.js
+++ 06-Workshop ExpressJS and Templating/movie-magic-home/src/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
 
 const app = express();
 const PORT = 5000;
 
+//setup handlebars
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs", //for setup extention name
+  })
+);
+app.set("view engine", "hbs"); //setup view engin
+app.set('views', path.join(__dirname, 'views')); //change the default behavior of folder views
+
+//setup express
 app.get("/", (req, res) => {
-  res.send("Home Page");
+  res.render('home', {layout: false})
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
