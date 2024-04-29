be67b29d586d022ee3313adc2751504aab6f0a17
Todor Krumov <baito6u@gmail.com>
2024-04-29 06:30:32 +0300

    Add create movie page

--- 06-Workshop ExpressJS and Templating/movie-magic-home/src/routes.js
+++ 06-Workshop ExpressJS and Templating/movie-magic-home/src/routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router(); // new instans of exxpress Router
 
 const homeController = require('./controllers/homeController')
+const movieController = require('./controllers/movieController')
 
 router.use(homeController);
+router.use(movieController);
 
 module.exports = router;
