334b7f2c390e095264e8ff2677ced3ad44b234a6
Todor Krumov <baito6u@gmail.com>
2024-04-29 04:44:32 +0300

    Setup express

--- 06-Workshop ExpressJS and Templating/movie-magic-home/src/index.js
+++ 06-Workshop ExpressJS and Templating/movie-magic-home/src/index.js
@@ -1,1 +1,11 @@
-console.log();
+const express = require("express");
+
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+console.log("Created by Todor Krumov");
