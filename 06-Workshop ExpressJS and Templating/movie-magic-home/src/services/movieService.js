3217efbed3f9366628a34ce4048e2524fd98debb
Todor Krumov <baito6u@gmail.com>
2024-04-30 06:04:47 +0300

    Add harcoded movie

--- 06-Workshop ExpressJS and Templating/movie-magic-home/src/services/movieService.js
+++ 06-Workshop ExpressJS and Templating/movie-magic-home/src/services/movieService.js
@@ -1,5 +1,16 @@
-const movies = [];
+const movies = [
+  {
+    title: "Jungle Cuise",
+    genre: "Adventure",
+    director: "Rob Marshall",
+    year: "2024",
+    image: "/img/jungle-cruise.jpeg",
+    rating: "5",
+    description: "Dreaming about saving countless lives and having another adventure, the feisty Englishfeminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change theworld. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant,wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine AmazonRiver in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper intothe heart of an impenetrable green maze, searching for something that cannot be found, acenturies-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to theirambitious plans.",
+  },
+];
 
 exports.create = (movieData) => {
     movies.push(movieData);
+    console.log(movieData);
 };
