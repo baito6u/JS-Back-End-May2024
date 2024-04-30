455dd20c8b8ca85b8eb7c5a0718fbcc8b7692cb3
Todor Krumov <baito6u@gmail.com>
2024-04-30 05:50:50 +0300

    Create movie action and service

--- 06-Workshop ExpressJS and Templating/movie-magic-home/src/controllers/movieController.js
+++ 06-Workshop ExpressJS and Templating/movie-magic-home/src/controllers/movieController.js
@@ -1,7 +1,16 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/create', (req, res) => {
-    res.render('create');
-})
+const movieService = require("../services/movieService");
+
+router.get("/create", (req, res) => {
+  res.render("create");
+});
+
+router.post("/create", (req, res) => {
+  const newMovie = req.body;
+  movieService.create(newMovie);
+
+  res.send("Movie should be created");
+});
 
 module.exports = router;
