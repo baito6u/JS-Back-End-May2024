dc29884c1dda7e06b6d15a8d23d2c0062d5f6fca
Todor Krumov <baito6u@gmail.com>
2024-04-30 05:49:55 +0300

    Add body parcer

--- 06-Workshop ExpressJS and Templating/movie-magic-home/src/config/expressConfig.js
+++ 06-Workshop ExpressJS and Templating/movie-magic-home/src/config/expressConfig.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
 
 function configExpress(app) {
-    //configure the static files in the public folder
-app.use(express.static("src/public")); //path.resolve('src/public'))
-
-    return app;
+  //configure the static files in the public folder
+  app.use(express.static("src/public")); //path.resolve('src/public'))
+  app.use(express.urlencoded({ extended: false })); //configuring middleware for parsing incoming request bodies from create form and make the data accessible via the req.body object. 
+  return app;
 }
 
 module.exports = configExpress;
