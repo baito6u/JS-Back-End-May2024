02dbf47b61f5154ff16b12727e2cb57f8c571c90
Todor Krumov <baito6u@gmail.com>
2024-05-12 01:25:38 +0300

    Show dropdown with cast in attach page

--- 08-Workshop MongoDB and Mongoose/movie-magic-home/src/controllers/movieController.js
+++ 08-Workshop MongoDB and Mongoose/movie-magic-home/src/controllers/movieController.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const movieService = require("../services/movieService");
+const castService = require("../services/castService");
 
 router.get("/create", (req, res) => {
   res.render("create");
@@ -29,7 +30,10 @@ router.get("/movies/:movieId", async (req, res) => {
 
 router.get("/movies/:movieId/attach", async (req, res) => {
   const movie = await movieService.getOne(req.params.movieId).lean();
-  res.render("movie/attach", { ...movie });
+  const casts = await castService.getAll().lean();
+
+  //TODO: remove alrady added casts
+  res.render("movie/attach", { ...movie, casts });
 });
 
 module.exports = router;
