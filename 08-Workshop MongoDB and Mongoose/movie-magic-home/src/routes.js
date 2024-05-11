05f3f8e5ad0887f26acb486b21015b326d0bbe17
Todor Krumov <baito6u@gmail.com>
2024-05-11 23:36:54 +0300

    Add create cast page

--- 08-Workshop MongoDB and Mongoose/movie-magic-home/src/routes.js
+++ 08-Workshop MongoDB and Mongoose/movie-magic-home/src/routes.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router(); // new instans of exxpress Router
 
-const homeController = require('./controllers/homeController')
-const movieController = require('./controllers/movieController')
+const homeController = require("./controllers/homeController");
+const movieController = require("./controllers/movieController");
+const castController = require("./controllers/castController");
 
 router.use(movieController);
 router.use(homeController);
+router.use('/cast', castController);
 
-router.get('*', (req, res) => {
-    res.redirect('/404');
+router.get("*", (req, res) => {
+  res.redirect("/404");
 });
 
 module.exports = router;
