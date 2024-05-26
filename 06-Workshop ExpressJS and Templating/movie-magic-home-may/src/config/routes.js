24787295839e0c3327653a3426d5ba8d10f66c62
Todor Krumov <baito6u@gmail.com>
2024-05-26 05:05:39 +0300

    added static views about, 404

--- 06-Workshop ExpressJS and Templating/movie-magic-home-may/src/config/routes.js
+++ 06-Workshop ExpressJS and Templating/movie-magic-home-may/src/config/routes.js
@@ -1,7 +1,11 @@
-const { homeContoller } = require("../controllers/homeController");
+const { homeContoller, aboutController, notFoundController } = require("../controllers/homeController");
 
 const router = require("express").Router();
 
-router.get('/', homeContoller)
+router.get('/', homeContoller);
+router.get('/about', aboutController);
+
+
+router.get('*', notFoundController)
 
 module.exports = { router };
--- 06-Workshop ExpressJS and Templating/movie-magic-home-may/src/controllers/homeController.js
+++ 06-Workshop ExpressJS and Templating/movie-magic-home-may/src/controllers/homeController.js
@@ -1,5 +1,11 @@
 module.exports = {
-    homeContoller: (req, res) => {
-        res.render('home');
-    }
-}
+  homeContoller: (req, res) => {
+    res.render("home");
+  },
+  aboutController: (req, res) => {
+    res.render("about");
+  },
+  notFoundController: (req, res) => {
+    res.render("404")
+  }
+};
