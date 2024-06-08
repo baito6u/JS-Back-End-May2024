44b92af31d813d023ea02576d23c350f3a7c1642
Todor Krumov <baito6u@gmail.com>
2024-06-08 06:24:38 +0300

    add a register page

--- 10-Workshop Session and Authentication/movie-magic-home-may-Part-3/src/config/routes.js
+++ 10-Workshop Session and Authentication/movie-magic-home-may-Part-3/src/config/routes.js
@@ -2,6 +2,8 @@ const { attachGetController, attachPostController } = require("../controllers/at
 const { createCastGetController, createCastPostController } = require("../controllers/castController");
 const { homeContoller, detailsController, createGetController, searchController, createPostController } = require("../controllers/movieController");
 const { aboutController, notFoundController } = require("../controllers/staticViewsController");
+const { registerGetController } = require("../controllers/userController");
+
 
 const router = require("express").Router();
 
@@ -15,6 +17,7 @@ router.post('/create/movie', createPostController);
 router.get('/create/cast', createCastGetController);
 router.post('/create/cast', createCastPostController);
 router.get('/search', searchController);
+router.get('/register', registerGetController);
 
 
 router.get('*', notFoundController)
---
+++ 10-Workshop Session and Authentication/movie-magic-home-may-Part-3/src/controllers/userController.js
@@ -0,0 +1,5 @@
+module.exports = {
+  registerGetController: (req, res) => {
+    res.render("register");
+  },
+};
