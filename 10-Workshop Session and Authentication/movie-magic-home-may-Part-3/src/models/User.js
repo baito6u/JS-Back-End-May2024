2c1485f21e275af9f75673f358bc628a967a8650
Todor Krumov <baito6u@gmail.com>
2024-06-08 05:53:24 +0300

    add case insensitive in User model

--- 10-Workshop Session and Authentication/movie-magic-home-may-Part-3/src/models/User.js
+++ 10-Workshop Session and Authentication/movie-magic-home-may-Part-3/src/models/User.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-    }
-});
+      type: String,
+      required: true,
+    },
+  },
+  {
+    collation: { locale: "en", strength: 2 },
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
