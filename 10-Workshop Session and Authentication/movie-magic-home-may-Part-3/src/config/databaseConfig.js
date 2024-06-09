986bb7ead98d05f7d88f47099456766ac10152b5
Todor Krumov <baito6u@gmail.com>
2024-06-09 16:01:10 +0300

    delete migration function

--- 10-Workshop Session and Authentication/movie-magic-home-may-Part-3/src/config/databaseConfig.js
+++ 10-Workshop Session and Authentication/movie-magic-home-may-Part-3/src/config/databaseConfig.js
@@ -1,22 +1,14 @@
 const mongoose = require("mongoose");
-const { Movie } = require("../models/Movie");
+require("../models/Movie");
 require("../models/Cast");
-const { User } = require("../models/User");
+require("../models/User");
 
 const connetionString = "mongodb://localhost:27017/movie-magic-2";
 
 async function databaseConfig() {
   await mongoose.connect(connetionString);
 
-  await migrateMovies();
-
   console.log("Database connected!");
 }
 
 module.exports = { databaseConfig };
-
-async function migrateMovies() {
-  const firstUser = await User.findOne();
-
-  await Movie.updateMany({}, { $set: { author: firstUser._id } });
-}
