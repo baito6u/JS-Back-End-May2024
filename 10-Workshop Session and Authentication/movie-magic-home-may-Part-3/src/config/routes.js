1fd9a0a8e4ea80b2da1b76b18023c1452778a62e
Todor Krumov <baito6u@gmail.com>
2024-06-09 17:12:52 +0300

    add guards

--- 10-Workshop Session and Authentication/movie-magic-home-may-Part-3/src/config/routes.js
+++ 10-Workshop Session and Authentication/movie-magic-home-may-Part-3/src/config/routes.js
@@ -25,22 +25,24 @@ const {
   logoutController,
 } = require("../controllers/userController");
 
+const { isGuest, isUser } = require("../middlewares/guards");
+
 const router = require("express").Router();
 
 router.get("/", homeContoller);
 router.get("/about", aboutController);
 router.get("/details/:id", detailsController);
-router.get("/attach/:id", attachGetController);
-router.post("/attach/:id", attachPostController);
-router.get("/create/movie", createGetController);
-router.post("/create/movie", createPostController);
-router.get("/create/cast", createCastGetController);
-router.post("/create/cast", createCastPostController);
+router.get("/attach/:id", isUser(), attachGetController);
+router.post("/attach/:id", isUser(), attachPostController);
+router.get("/create/movie", isUser(), createGetController);
+router.post("/create/movie", isUser(), createPostController);
+router.get("/create/cast", isUser(), createCastGetController);
+router.post("/create/cast", isUser(), createCastPostController);
 router.get("/search", searchController);
-router.get("/register", registerGetController);
-router.post("/register", registerPostController);
-router.get("/login", loginGetController);
-router.post("/login", loginPostController);
+router.get("/register", isGuest(), registerGetController);
+router.post("/register", isGuest(), registerPostController);
+router.get("/login", isGuest(), loginGetController);
+router.post("/login", isGuest(), loginPostController);
 router.get("/logout", logoutController);
 
 router.get("*", notFoundController);
