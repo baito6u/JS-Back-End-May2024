29c024d4126da43d035150c9d68312cc732b7034
Todor Krumov <baito6u@gmail.com>
2024-06-14 04:33:45 +0300

    add routes module

--- 13-Exam Preparation/CourseBook-Papazov/index.js
+++ 13-Exam Preparation/CourseBook-Papazov/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.static('static'));
+app.use(express.urlencoded({extended: false}));
+
+app.use(routes);
 
 app.get('/', (req, res) => {
     res.send("Hello world");
