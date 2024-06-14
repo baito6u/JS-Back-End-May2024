2847034703d2476d82a6dd14699eeef935f4a627
Todor Krumov <baito6u@gmail.com>
2024-06-14 06:17:22 +0300

    add register functionality to create user in DB

--- 13-Exam Preparation/CourseBook-Papazov/controllers/authController.js
+++ 13-Exam Preparation/CourseBook-Papazov/controllers/authController.js
@@ -1,7 +1,17 @@
+const { register } = require('../services/authService');
+
 const authRouter = require('express').Router();
 
 authRouter.get('/register', (req, res) => {
     res.render('register');
+});
+
+authRouter.post('/register', async (req, res) => {
+    const userData = req.body;
+
+    await register(userData);
+
+    res.redirect('/login');
 });
 
 module.exports = authRouter;
--- 13-Exam Preparation/CourseBook-Papazov/models/User.js
+++ 13-Exam Preparation/CourseBook-Papazov/models/User.js
@@ -1,21 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+userSchema.virtual("repass").set(function (value) {
+  if (value !== this.password) {
+    throw new Error("Password missmth");
+  }
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
