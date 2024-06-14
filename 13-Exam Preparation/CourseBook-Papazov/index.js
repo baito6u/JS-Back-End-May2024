b338caf012a9e5b02bcc91350a31243636922249
Todor Krumov <baito6u@gmail.com>
2024-06-14 04:19:15 +0300

    install express and start server

--- 13-Exam Preparation/CourseBook-Papazov/index.js
+++ 13-Exam Preparation/CourseBook-Papazov/index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+});
+
+app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
