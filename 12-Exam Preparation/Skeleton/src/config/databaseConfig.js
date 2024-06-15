e98525a66390702297347b5f33bc79da356b471c
Todor Krumov <baito6u@gmail.com>
2024-06-15 05:58:50 +0300

    Create databaseConfig.js

---
+++ 12-Exam Preparation/Skeleton/src/config/databaseConfig.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+//TODO importmodels
+
+async function databaseConfig() {
+  //TODO change DB name
+  const connectionString = "mongodb://localhost:27017/exam-db";
+
+  mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  console.log("Database connected");
+}
