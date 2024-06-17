0d62c5b5f43028c703b0fda263d38c5c11735b71
Todor Krumov <baito6u@gmail.com>
2024-06-17 06:52:02 +0300

    Update homeController.js

--- 12-Exam Preparation/EarthTreasureVaul/src/controllers/homeController.js
+++ 12-Exam Preparation/EarthTreasureVaul/src/controllers/homeController.js
@@ -5,12 +5,7 @@ const homeRouter = require("express").Router();
 
 //TODO replace with real router according exam description
 homeRouter.get("/", async (req, res) => {
-    console.log(req.user);
-
-    //This code creates a token and save it in a cookie
-    // const result = await login("peter", "123456");
-    // const token = createToken(result);
-    // res.cookie("token", token);
+    
 
     res.render("home");
 })
