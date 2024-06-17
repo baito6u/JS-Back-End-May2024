10e8ffa04e5a83181d40899d6593ef11f9f4386b
Todor Krumov <baito6u@gmail.com>
2024-06-17 06:18:54 +0300

    Create dataService.js

---
+++ 12-Exam Preparation/Skeleton/src/services/dataService.js
@@ -0,0 +1,65 @@
+const { Data } = require("../models/Data");
+
+//TODO replace with real data service according exam description
+
+async function getAll() {
+  return Data.find().lean();
+}
+
+async function getById(id) {
+  return Data.findById(id).lean();
+}
+
+async function create(data, authorId) {
+  //TODO extract properties from Data model
+
+  const record = new Data({
+    prop: data.prop,
+    author: authorId,
+  });
+
+  await record.save();
+
+  return record;
+}
+
+async function update(id, data, userId) {
+  const record = await Data.findById(id);
+
+  if (!record) {
+    throw new ReferenceError("Record not found!" + id);
+  }
+
+  if(record.author.toString() != userId) {
+    throw new Error("Access denied!");
+  }
+
+  //TODO replace with real propperties
+  record.prop = data.prop;
+
+  await record.save();
+
+  return record;
+}
+
+async function deleteById(id, userId) {
+    const record = await Data.findById(id);
+
+  if (!record) {
+    throw new ReferenceError("Record not found!" + id);
+  }
+
+  if(record.author.toString() != userId) {
+    throw new Error("Access denied!");
+  }
+
+  await Data.findByIdAndDelete(id);
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    deleteById,
+}
