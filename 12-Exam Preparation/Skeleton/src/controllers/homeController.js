3ef5353705729632cbb22a52e86fab620e482d9b
Todor Krumov <baito6u@gmail.com>
2024-06-17 05:54:36 +0300

    Update homeController.js

--- 12-Exam Preparation/Skeleton/src/controllers/homeController.js
+++ 12-Exam Preparation/Skeleton/src/controllers/homeController.js
@@ -1,8 +1,16 @@
+const { createToken } = require("../services/jwt");
+const { login } = require("../services/userService");
+
 const homeRouter = require("express").Router();
 
 //TODO replace with real router according exam description
-homeRouter.get("/", (req, res) => {
+homeRouter.get("/", async (req, res) => {
     console.log(req.user);
+
+    //This code creates a token and save it in a cookie
+    // const result = await login("peter", "123456");
+    // const token = createToken(result);
+    // res.cookie("token", token);
 
     res.render("home");
 })
