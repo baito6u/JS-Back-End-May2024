eb6d90fc07a3dc37dfc3ea75b9d2eee4b5525579
Todor Krumov <baito6u@gmail.com>
2024-06-17 06:52:07 +0300

    Create userController.js

---
+++ 12-Exam Preparation/EarthTreasureVaul/src/controllers/userController.js
@@ -0,0 +1,4 @@
+//This code creates a token and save it in a cookie
+    // const result = await login("peter", "123456");
+    // const token = createToken(result);
+    // res.cookie("token", token);
