3533c5818f60eb632c4499c82aacd199a201a0e4
Todor Krumov <baito6u@gmail.com>
2024-06-17 05:45:05 +0300

    Update expressConfig.js

--- 12-Exam Preparation/Skeleton/src/config/expressConfig.js
+++ 12-Exam Preparation/Skeleton/src/config/expressConfig.js
@@ -1,8 +1,10 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 
+const secret = "cookie secret";
+
 function expressConfig(app) {
-  app.use(cookieParser);
+  app.use(cookieParser(secret));
   //TODO add session middlware
 
   app.use("/static", express.static("static"));
