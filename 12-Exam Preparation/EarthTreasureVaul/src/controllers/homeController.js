dbec961bfe45e3a42ffc4557c2b3119879df7923
Todor Krumov <baito6u@gmail.com>
2024-06-18 06:02:03 +0300

    Update homeController.js

--- 12-Exam Preparation/EarthTreasureVaul/src/controllers/homeController.js
+++ 12-Exam Preparation/EarthTreasureVaul/src/controllers/homeController.js
@@ -1,11 +1,13 @@
+const { getRecent } = require("../services/stoneService");
+
 const homeRouter = require("express").Router();
 
 //TODO replace with real router according exam description
 
-homeRouter.get("/", (req, res) => {
-  const minerals = [];
+homeRouter.get("/", async (req, res) => {
+  const stones = await getRecent();
 
-  res.render("home", { minerals });
+  res.render("home", { stones });
 });
 
 module.exports = { homeRouter };
