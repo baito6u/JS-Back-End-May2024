79e56f702b67ce0946bc59fcaf59359143889e43
Todor Krumov <baito6u@gmail.com>
2024-06-18 06:19:51 +0300

    Update routesConfig.js

--- 12-Exam Preparation/EarthTreasureVaul/src/config/routesConfig.js
+++ 12-Exam Preparation/EarthTreasureVaul/src/config/routesConfig.js
@@ -1,10 +1,12 @@
 //TODO imprt routers
 
 const { homeRouter } = require("../controllers/homeController");
+const { userRouter } = require("../controllers/userController");
 
 function routesConfig(app) {
   //TODO register routers
   app.use(homeRouter);
+  app.use(userRouter);
 }
 
 module.exports = { routesConfig };
