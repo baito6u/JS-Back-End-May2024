be9a4905ab9802096fc97a39da3c8a8ac44ad953
Todor Krumov <baito6u@gmail.com>
2024-06-18 05:07:22 +0300

    add partial mineral

--- 12-Exam Preparation/EarthTreasureVaul/src/controllers/homeController.js
+++ 12-Exam Preparation/EarthTreasureVaul/src/controllers/homeController.js
@@ -3,7 +3,7 @@ const homeRouter = require("express").Router();
 //TODO replace with real router according exam description
 
 homeRouter.get("/", (req, res) => {
-  const minerals = [1, 2, 3, 4];
+  const minerals = [];
 
   res.render("home", { minerals });
 });
