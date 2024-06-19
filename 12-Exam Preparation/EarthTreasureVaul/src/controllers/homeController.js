d23b4d6ae7a05458e398d206f8fe26bd7f945a4e
Todor Krumov <baito6u@gmail.com>
2024-06-19 10:16:56 +0300

    add catalog view

--- 12-Exam Preparation/EarthTreasureVaul/src/controllers/homeController.js
+++ 12-Exam Preparation/EarthTreasureVaul/src/controllers/homeController.js
@@ -1,11 +1,17 @@
 const homeRouter = require("express").Router();
 
-const { getRecent } = require("../services/stoneService");
+const { getRecent, getAll } = require("../services/stoneService");
 
 homeRouter.get("/", async (req, res) => {
   const stones = await getRecent();
 
   res.render("home", { stones });
+});
+
+homeRouter.get("/catalog", async (req, res) => {
+  const stones = await getAll();
+
+  res.render("catalog", { stones });
 });
 
 module.exports = { homeRouter };
