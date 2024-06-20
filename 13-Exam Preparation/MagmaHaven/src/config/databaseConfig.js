573ded3911ebbb390112328b289ae7e7c74f9c7b
Todor Krumov <baito6u@gmail.com>
2024-06-20 20:15:35 +0300

    Update databaseConfig.js

--- 13-Exam Preparation/MagmaHaven/src/config/databaseConfig.js
+++ 13-Exam Preparation/MagmaHaven/src/config/databaseConfig.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
-require("../models/Data"); //TODO import real models 
+require("../models/Data"); //TODO import real models
 require("../models/User");
 
 async function databaseConfig() {
-  //TODO change DB name
-  const connectionString = "mongodb://localhost:27017/exam-db";
+  const connectionString = "mongodb://localhost:27017/magma-haven";
 
   mongoose.connect(connectionString);
 
