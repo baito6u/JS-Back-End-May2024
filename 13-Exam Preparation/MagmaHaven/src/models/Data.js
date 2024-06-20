ef32de10c930ef52683df40c2ce98359c253fdac
Todor Krumov <baito6u@gmail.com>
2024-06-20 21:22:49 +0300

    Update Data.js

--- 13-Exam Preparation/MagmaHaven/src/models/Data.js
+++ 13-Exam Preparation/MagmaHaven/src/models/Data.js
@@ -1,17 +1,44 @@
 const mongoose = require("mongoose");
 
-
-//TODO replace with dataa model from exam description
-
 const dataSchema = new mongoose.Schema({
-    prop: {
-        type: String,
-        required: true,
-    },
-    author: {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  elevation: {
+    type: Number,
+    required: true,
+  },
+  lastEruption: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  typeVolcano: {
+    type: String,
+    enum: ["Supervolcanoes", "Submarine", "Subglacial", "Mud", "Stratovolcanoes", "Shield"],
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  voteList: {
+    type: [mongoose.Types.ObjectId],
+    ref: "User",
+    default: [],
+  },
+  author: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const Data = mongoose.model("Data", dataSchema);
