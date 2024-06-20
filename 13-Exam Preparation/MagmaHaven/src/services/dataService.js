bf5d106192157ad29668d235d6a5421442733342
Todor Krumov <baito6u@gmail.com>
2024-06-20 21:22:56 +0300

    Update dataService.js

--- 13-Exam Preparation/MagmaHaven/src/services/dataService.js
+++ 13-Exam Preparation/MagmaHaven/src/services/dataService.js
@@ -1,7 +1,5 @@
 const { Data } = require("../models/Data");
 
-//TODO replace with real data service according exam description
-
 async function getAll() {
   return Data.find().lean();
 }
@@ -14,7 +12,13 @@ async function create(data, authorId) {
   //TODO extract properties from Data model
 
   const record = new Data({
-    prop: data.prop,
+    name: data.name,
+    location: data.location,
+    elevation: data.elevation,
+    lastEruption: data.lastEruption,
+    image: data.image,
+    typeVolcano: data.typeVolcano,
+    description: data.description,
     author: authorId,
   });
 
@@ -30,26 +34,34 @@ async function update(id, data, userId) {
     throw new ReferenceError("Record not found!" + id);
   }
 
-  if(record.author.toString() != userId) {
+  if (record.author.toString() != userId) {
     throw new Error("Access denied!");
   }
 
-  //TODO replace with real propperties
-  record.prop = data.prop;
+  record.name = data.name;
+  record.location = data.location;
+  record.elevation = data.elevation;
+  record.lastEruption = data.lastEruption;
+  record.image = data.image;
+  record.typeVolcano = data.typeVolcano;
+  record.description = data.description;
+  record.author = authorId;
 
   await record.save();
 
   return record;
 }
 
+//TODO add voting method
+
 async function deleteById(id, userId) {
-    const record = await Data.findById(id);
+  const record = await Data.findById(id);
 
   if (!record) {
     throw new ReferenceError("Record not found!" + id);
   }
 
-  if(record.author.toString() != userId) {
+  if (record.author.toString() != userId) {
     throw new Error("Access denied!");
   }
 
@@ -57,9 +69,9 @@ async function deleteById(id, userId) {
 }
 
 module.exports = {
-    getAll,
-    getById,
-    create,
-    update,
-    deleteById,
-}
+  getAll,
+  getById,
+  create,
+  update,
+  deleteById,
+};
