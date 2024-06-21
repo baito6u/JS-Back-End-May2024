8552cf09350f0a4a3e950886db5d749bd00dafeb
Todor Krumov <baito6u@gmail.com>
2024-06-21 06:45:14 +0300

    add register and login

--- 13-Exam Preparation/MagmaHaven/src/controllers/userController.js
+++ 13-Exam Preparation/MagmaHaven/src/controllers/userController.js
@@ -1,20 +1,75 @@
 const userRouter = require("express").Router();
+const { body, validationResult } = require("express-validator");
 
 const { isGuest } = require("../middlewares/guards");
 const { createToken } = require("../services/jwt");
-const { login } = require("../services/userService");
-
-//This code creates a token and save it in a cookie
-// const result = await login("peter", "123456");
-// const token = createToken(result);
-// res.cookie("token", token);
+const { login, register } = require("../services/userService");
+const { parseError } = require("../utils");
 
 userRouter.get("/login", isGuest(), (req, res) => {
   res.render("login");
 });
 
+userRouter.post(
+  "/login",
+  isGuest(),
+  body("email").trim(),
+  body("password").trim(),
+  async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      const result = await login(email, password);
+      const token = createToken(result);
+      res.cookie("token", token);
+
+      res.redirect("/");
+    } catch (err) {
+      res.render("login", { data: { email }, errors: parseError(err).errors });
+    }
+  }
+);
+
+userRouter.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.redirect("/");
+});
+
 userRouter.get("/register", isGuest(), (req, res) => {
   res.render("register");
 });
+
+userRouter.post(
+  "/register",
+  isGuest(),
+  body("username").trim().isLength({ min: 2 }),
+  body("email").trim().isLength({ min: 10 }).isEmail(),
+  body("password").trim().isLength({ min: 4 }),
+  body("repas")
+    .trim()
+    .custom((value, { req }) => value == req.body.password)
+    .withMessage("Passwords don't match!"),
+  async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+      const validation = validationResult(req);
+
+      if (validation.errors.isLength) {
+        throw validation.errors;
+      }
+      const result = await register(username, email, password);
+      const token = createToken(result);
+      res.cookie("token", token);
+
+      res.redirect("/");
+    } catch (err) {
+      res.render("register", {
+        data: { username, email },
+        errors: parseError(err).errors,
+      });
+    }
+  }
+);
 
 module.exports = { userRouter };
