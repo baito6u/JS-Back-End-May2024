f8baf352e5b07dc4dbe07a9cef4ed3d4b808a3cd
Todor Krumov <baito6u@gmail.com>
2024-06-21 04:40:59 +0300

    Update homeController.js

--- 13-Exam Preparation/MagmaHaven/src/controllers/homeController.js
+++ 13-Exam Preparation/MagmaHaven/src/controllers/homeController.js
@@ -1,18 +1,7 @@
-const { createToken } = require("../services/jwt");
-const { login } = require("../services/userService");
-
 const homeRouter = require("express").Router();
 
-//TODO replace with real router according exam description
 homeRouter.get("/", async (req, res) => {
-    console.log(req.user);
-
-    //This code creates a token and save it in a cookie
-    // const result = await login("peter", "123456");
-    // const token = createToken(result);
-    // res.cookie("token", token);
-
-    res.render("home");
-})
+  res.render("home");
+});
 
 module.exports = { homeRouter };
