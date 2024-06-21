9b5904b24f7fe95a3e4de4f56975734e17812691
Todor Krumov <baito6u@gmail.com>
2024-06-21 05:27:02 +0300

    add dynamic catalog page

--- 13-Exam Preparation/MagmaHaven/src/controllers/catalogController.js
+++ 13-Exam Preparation/MagmaHaven/src/controllers/catalogController.js
@@ -1,7 +1,11 @@
 const catalogRouter = require("express").Router();
 
-catalogRouter.get("/catalog", (req, res) => {
-  res.render("catalog");
+const { getAll } = require("../services/dataService");
+
+catalogRouter.get("/catalog", async (req, res) => {
+  const volcanoes = await getAll();
+
+  res.render("catalog", { volcanoes });
 });
 
 module.exports = { catalogRouter };
