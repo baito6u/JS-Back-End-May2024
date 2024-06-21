e79924eaf588c1230699691111e4e977dbc1d376
Todor Krumov <baito6u@gmail.com>
2024-06-21 04:51:36 +0300

    Create userController.js

---
+++ 13-Exam Preparation/MagmaHaven/src/controllers/userController.js
@@ -0,0 +1,7 @@
+const { createToken } = require("../services/jwt");
+const { login } = require("../services/userService");
+
+//This code creates a token and save it in a cookie
+    // const result = await login("peter", "123456");
+    // const token = createToken(result);
+    // res.cookie("token", token);
