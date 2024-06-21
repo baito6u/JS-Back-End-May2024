cd2fbdad9f0505d6b898e0baed0b955b3514b5cd
Todor Krumov <baito6u@gmail.com>
2024-06-21 05:58:56 +0300

    add login and register views

--- 13-Exam Preparation/MagmaHaven/src/config/routesConfig.js
+++ 13-Exam Preparation/MagmaHaven/src/config/routesConfig.js
@@ -2,11 +2,13 @@
 
 const { catalogRouter } = require("../controllers/catalogController");
 const { homeRouter } = require("../controllers/homeController");
+const { userRouter } = require("../controllers/userController");
 
 function routesConfig(app) {
   //TODO register routers
   app.use(homeRouter);
   app.use(catalogRouter);
+  app.use(userRouter);
 }
 
 module.exports = { routesConfig };
--- 13-Exam Preparation/MagmaHaven/src/controllers/userController.js
+++ 13-Exam Preparation/MagmaHaven/src/controllers/userController.js
@@ -1,7 +1,20 @@
+const userRouter = require("express").Router();
+
+const { isGuest } = require("../middlewares/guards");
 const { createToken } = require("../services/jwt");
 const { login } = require("../services/userService");
 
 //This code creates a token and save it in a cookie
-    // const result = await login("peter", "123456");
-    // const token = createToken(result);
-    // res.cookie("token", token);
+// const result = await login("peter", "123456");
+// const token = createToken(result);
+// res.cookie("token", token);
+
+userRouter.get("/login", isGuest(), (req, res) => {
+  res.render("login");
+});
+
+userRouter.get("/register", isGuest(), (req, res) => {
+  res.render("register");
+});
+
+module.exports = { userRouter };
