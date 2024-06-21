1e07e13bbc2608aab221ec33222c5ce77d2815a1
Todor Krumov <baito6u@gmail.com>
2024-06-21 05:46:30 +0300

    Update catalogController.js

--- 13-Exam Preparation/MagmaHaven/src/controllers/catalogController.js
+++ 13-Exam Preparation/MagmaHaven/src/controllers/catalogController.js
@@ -1,11 +1,27 @@
 const catalogRouter = require("express").Router();
 
-const { getAll } = require("../services/dataService");
+const { getAll, getById } = require("../services/dataService");
 
 catalogRouter.get("/catalog", async (req, res) => {
   const volcanoes = await getAll();
 
   res.render("catalog", { volcanoes });
+});
+
+catalogRouter.get("/catalog/:id", async (req, res) => {
+  const id = req.params.id;
+  const volcano = await getById(id);
+
+  if (!volcano) {
+    res.status(404).render("404");
+    return;
+  }
+
+  volcano.votes = volcano.voteList.length;
+  volcano.hasUser = res.locals.hasUser;
+  //TODO add isAuthor and hasVoted properties
+
+  res.render("details", { volcano });
 });
 
 module.exports = { catalogRouter };
