b4723113c2a221d90068083c48d7fb41ecd9180e
Todor Krumov <baito6u@gmail.com>
2024-06-22 05:04:01 +0300

    Update routesConfig.js

--- 13-Exam Preparation/MagmaHaven/src/config/routesConfig.js
+++ 13-Exam Preparation/MagmaHaven/src/config/routesConfig.js
@@ -1,14 +1,17 @@
-//TODO imprt routers
-
-const { catalogRouter } = require("../controllers/catalogController");
 const { homeRouter } = require("../controllers/homeController");
+const { catalogRouter } = require("../controllers/catalogController");
 const { userRouter } = require("../controllers/userController");
+const { volcanoRouter } = require("../controllers/volcanoController");
 
 function routesConfig(app) {
-  //TODO register routers
   app.use(homeRouter);
   app.use(catalogRouter);
   app.use(userRouter);
+  app.use(volcanoRouter);
+
+  app.get("*", (req, res) => {
+    res.render("404");
+  });
 }
 
 module.exports = { routesConfig };
