0569eb1ab4b6c5236d0d14cec08a137a8332137e
Todor Krumov <baito6u@gmail.com>
2024-06-26 05:55:23 +0300

    Update userController.js

--- 14-Workshop REST API/REST-API/Server/controllers/userController.js
+++ 14-Workshop REST API/REST-API/Server/controllers/userController.js
@@ -1,7 +1,17 @@
+const { register } = require("../services/userService");
+
 const userRouter = require("express").Router();
 
-userRouter.post("/register", (req, res) => {
-    
+userRouter.post("/register", async (req, res) => {
+  const userData = req.body;
+
+  const { _id, email, token } = await register(userData);
+
+  res.json({
+    _id,
+    email,
+    accessToken: token,
+  });
 });
 
 module.exports = userRouter;
