542c1f550e032c34d76e6eb84c97d50002e1eab9
Todor Krumov <baito6u@gmail.com>
2024-06-26 05:55:59 +0300

    hash password

--- 14-Workshop REST API/REST-API/Server/models/User.js
+++ 14-Workshop REST API/REST-API/Server/models/User.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+});
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 12);
 });
 
 const User = mongoose.model("User", userSchema);
