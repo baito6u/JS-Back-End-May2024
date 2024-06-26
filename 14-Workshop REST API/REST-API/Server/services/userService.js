d3d27363f8295b761014f0c574cb0614e130e921
Todor Krumov <baito6u@gmail.com>
2024-06-26 05:56:18 +0300

    create register

--- 14-Workshop REST API/REST-API/Server/services/userService.js
+++ 14-Workshop REST API/REST-API/Server/services/userService.js
@@ -1,3 +1,26 @@
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/User");
 
-exports.register = (userData) => User.create(userData);
+exports.register = async (userData) => {
+  //   if (userData.password !== userData.rePass) {
+  //     throw new Error("Password don't match!");
+  //   }
+
+  const user = await User.create(userData);
+
+  const payload = {
+    _id: user._id,
+    email: user.email,
+  };
+
+  const SECRET = "DQWDKIJ3DJIASKSDLjo3ijf23jrj29"
+
+  const token = jwt.sign(payload, SECRET);
+
+  return {
+    _id: user._id,
+    email: user.email,
+    token: token,
+  };
+};
