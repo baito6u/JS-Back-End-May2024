f24b3514d4b7dd97c7323e7707a632af6cb3b1d3
Todor Krumov <baito6u@gmail.com>
2024-06-28 04:33:06 +0300

    add details page

--- 14-Workshop REST API/REST-API/Server/controllers/catalogController.js
+++ 14-Workshop REST API/REST-API/Server/controllers/catalogController.js
@@ -3,23 +3,22 @@ const { getAll, create, getOne } = require("../services/dataService");
 const catalogRouter = require("express").Router();
 
 catalogRouter.get("/catalog", async (req, res) => {
-    const data = await getAll();
+  const data = await getAll();
 
-    res.json(data);
+  res.json(data);
 });
 
-catalogRouter.get("/:dataId", async (req, res) => {
-    const singleData = await getOne(req.params.dataId);
-
-    res.json(singleData);
-})
-
-catalogRouter.post("/", async (req, res) => {
+catalogRouter.post("/catalog", async (req, res) => {
   const data = req.body;
-
+  
   const furnitures = await create(data);
-
+  
   res.json(furnitures);
 });
 
+catalogRouter.get("/catalog/:dataId", async (req, res) => {
+  const singleData = await getOne(req.params.dataId);
+
+  res.json(singleData);
+});
 module.exports = catalogRouter;
